f098948d89059b5c9b5b629814195b68d29f2ab9
Harshit Mishra <harshitmishra172004@gmail.com>
2026-06-04 03:14:07 +0530

    Implement left arrow pattern and add right arrow pattern program

--- left_arrow_pattern.cpp
+++ left_arrow_pattern.cpp
@@ -4,25 +4,17 @@ using namespace std;
 int main() {
     int n = 5;
 
-    
-    for (int i = 0; i < n; i++) {
-        // Print leading spaces
-        for (int j = 0; j < i; j++) {
-            cout << "  "; 
-        }
-        // Print stars
-        for (int k = 0; k < n - i; k++) {
+    // Upper part
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
             cout << "*";
         }
         cout << endl;
     }
-    for (int i = 1; i < n; i++) {
-     
-        for (int j = 0; j < n - i - 1; j++) {
-            cout << "  ";
-        }
-        // Print stars
-        for (int k = 0; k < i + 1; k++) {
+
+    // Lower part
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
             cout << "*";
         }
         cout << endl;
---
+++ right_arrow.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int j = 0; j < i; j++) {
+            cout << "  "; 
+        }
+        // Print stars
+        for (int k = 0; k < n - i; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i < n; i++) {
+     
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << "  ";
+        }
+        
+        for (int k = 0; k < i + 1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
