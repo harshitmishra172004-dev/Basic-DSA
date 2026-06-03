9cf0661d38298cf0409a80113859fe17ce614c71
Harshit Mishra <harshitmishra172004@gmail.com>
2026-06-04 03:05:12 +0530

    Refactor code structure for improved readability and maintainability

--- inverted_Num_pyramid.cpp
+++ inverted_Num_pyramid.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
-    int n;
+    int n=5;
     cout<<"Enter the Number: ";
     cin>>n;
 
-    for(int i=1;i<=n;i++){
+    for(int i=n;i>=1;i++){
         for(int j=1;j<i;j++){
             cout<<"  ";
             }
