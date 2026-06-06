c9a5fe809cbdb6731b3a7bb315c0145ba3ce32d5
Harshit Mishra <harshitmishra172004@gmail.com>
2026-06-07 05:23:02 +0530

    Add program to calculate Fibonacci numbers

---
+++ Day 3/fibonacci.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int fab(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fab(n-1) + fab(n-2);
+}
+    int main(){
+        int n ;
+        cout<<"Enter the number :";
+        cin>>n;
+        cout<<"Fibonacci of number is: "<<fab(n);
+        return 0;
+
+    }
