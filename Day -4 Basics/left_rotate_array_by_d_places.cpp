3b567b8d48dbf64de7cb3637f8ca1847e4dcfd3d
Harshit Mishra <harshitmishra172004@gmail.com>
2026-06-13 02:32:01 +0530

    Add program to left rotate an array by d places

---
+++ Day -4 Basics/left_rotate_array_by_d_places.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,d;
+    cin>>n>>d;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<d;i++){
+        int temp=arr[0];
+        for(int j=0;j<n-1;j++){
+            arr[j]=arr[j+1];
+        }
+        arr[n-1]=temp;
+    }
+
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
---
+++ Day -4 Basics/move_all_zeroes_to_the_end.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            swap(arr[count], arr[i]);
+            count++;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
