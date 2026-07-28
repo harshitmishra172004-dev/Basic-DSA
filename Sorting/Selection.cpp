630c34e53060fe9af5dca6bab50a413279b92990
Harshit Mishra <harshitmishra172004@gmail.com>
2026-07-29 01:39:58 +0530

    Add C++ build task configuration and implement Bubble and Selection sort algorithms

---
+++ Sorting/Selection.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[6]={10,3,17,23,27,11};
+
+    for(int i=0;i<5;i++){
+        int index=i;
+        for(int j=i+1;j<6;j++){
+            if(arr[j]<arr[index])
+                index=j;
+        }
+        swap(arr[i],arr[index]);
+    }
+    for(int i=0;i<6;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
